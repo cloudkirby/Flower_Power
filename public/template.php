2de7e94956bf0a0433a7ef86cdc704e33a10523e
Anthony <ff_crazed@yahoo.com>
2016-02-27 22:26:53 +0000

    Fix styles.

--- public/template.php
+++ public/template.php
@@ -1,25 +1,17 @@
 <!DOCTYPE html>
 <html lang ="en">
-
 <head>
-
-	<meta charset=""utf-8">
+	<meta charset="utf-8">
 	<title>Flower Power </title>
 	<meta name="description" content="Flowerpower">
-	
 	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-	
 	<!-- Optional theme -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
-	
 	<link rel="stylesheet" type="text/css" href="css.css">
-	
 </head>
-
 <body>
-
-	<nav class="navbar navbar-inverse navbar-fixed-top" id="navbar">
+	<nav class="navbar navbar-inverse navbar-fixed-top">
 		<div class="container">
 			<div class="navbar-header">
 			<a href="" class="navbar-brand">Flowerpower</a>
@@ -28,21 +20,17 @@
 			</ul>
 		</div>
 	</nav>
-
 	<div class="jumbotron">
 		<div class="container text-center">
-		<h1>Flower Power</h1>
-		<p></p>
-		
-		<div class ="btn-group">
-			<a href="" class="btn btn-lg btn-warning">placerholder</a>
+			<h1>Flower Power</h1>
+			<p>placeholder</p>
+			<div class ="btn-group">
+				<a href="" class="btn btn-lg btn-warning">placeholder</a>
+			</div>
 		</div>
-		
-		</div>
-	
+	</div>
 	<script src=""http://code.jquery.com/jquery-2.2.1.min.js"">
 	<!-- Latest compiled and minified JavaScript -->
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-
 </body>
 </html>	

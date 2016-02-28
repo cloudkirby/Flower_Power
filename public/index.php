12310b9dbbd2d7868be485c697bdf6cecda8fa1a
Anthony <ff_crazed@yahoo.com>
2016-02-28 18:40:28 +0000

    Cleanup

--- public/index.php
+++ public/index.php
@@ -1,19 +1,18 @@
 <?php
-
 function getError($url)
 {
 	$headers = get_headers($url,1);
 
 	if ($headers[0] == 'HTTP/1.1 200 OK')
 	{
-		$url = $url . "'";
+		$errorUrl = $url . "'";
 
-		$contents = file_get_contents($url);
+		$contents = file_get_contents($errorUrl);
 
 		return $contents;
 	}
 
 	return NULL;
 }
-var_dump(getError("http://www.cideko.com/pro_con.php?id=3"));
+var_dump(getError("http://www.bible-history.com/subcat.php?id=22"));
 ?>

